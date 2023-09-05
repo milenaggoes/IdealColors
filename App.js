68ff6c104dd63b1a82d64f06ba6030e1dc389420
milenaggoes <rm94305@fiap.com.br>
2023-09-05 16:27:53 -0300

    iniciando projeto

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
+import logo from "./assets/logo.png";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        paddingHorizontal: 10,
+        paddingVertical: 20,
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "center",
+      }}
+    >
+      <MaterialIcons name="arrow-back" style={{ alignSelf: "flex-start"}} />
+      <Image source={logo} style={{ width: "90%", height: "38%" }} />
+      <Text style={{fontSize: 27, fontWeight: "500",lineHeight: 34, textAlign: "center"}}>
+        Saiba as cores{"\n"}que te favorecem
+      </Text>
+      <Text style={{ fontSize: 16, fontWeight: "400", textAlign: "center", lineHeight: 18}}>
+        Indentificaremos sua tonalidade de pele e mostraremos as cores que
+        valorizam a sua beleza natural
+      </Text>
+      <TouchableOpacity style={{width:300, paddingVertical: 23, marginTop:85, backgroundColor: "#69B2D1",borderRadius: 20, alignItems: "center"}}>
+        <Text style={{ fontSize: 16,fontWeight: "600", color: "#ffffff",}}>Vamos Começar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
